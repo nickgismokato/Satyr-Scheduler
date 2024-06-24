eda09a763dfe9e1cdac8bf1fd96a9f43846112dd
nickgismokato <nickvillumlaursen@gmail.com>
2024-06-24 13:05:52 +0200

    Reworked the packages.

--- docs/example/simple/Main.py
+++ docs/example/simple/Main.py
@@ -1,8 +1,12 @@
 #remember to do: "pip install SatyrScheduler"
 import SatyrScheduler as ss
 
+import os
 
 def Main():
+	a = ss.GetData()
+	for obj in a:
+		print(obj.df)
 	return None
 
 if __name__ == "__main__":
