12516cc578b143277dd6e35c87f9eed2c370062e
nickgismokato <nickvillumlaursen@gmail.com>
2024-07-26 11:18:54 +0200

    Adding more documentation.

--- docs/example/simple/Main.py
+++ docs/example/simple/Main.py
@@ -9,11 +9,12 @@ def Main():
 		print(f"Object name: [{obj.name}]\nObject Directory: [{obj.dir}]\nObject TID: [{obj.TID}]")
 		if(obj.TID == 1):
 			for sketch in obj.lstSketchObj:
-				print(f"\tSketch: {sketch.title}\tRanking: {sketch.ranking}\tInstructor: {sketch.instructor}\t People: {sketch.people}")
+				print(f"\tSketch: {sketch.title}\tRanking: {sketch.ranking}\tInstructor: {sketch.instructor}\t People: {sketch.people}\t Need Instructor: {sketch.need_Instructor}")
 		if(obj.TID == 0):
 			for song in obj.lstSongObj:
-				print(f"\tSong: {song.title}\tRanking: {song.ranking}\tInstructor: {song.instructor}\t People: {song.people}")
+				print(f"\tSong: {song.title}\tRanking: {song.ranking}\tInstructor: {song.instructor}\t People: {song.people}\t Need Instructor: {song.need_Instructor}")
 		print("---------------------------------")
+	ss.InitSchedule(breaktime = 10, hello = "World!")
 	return None
 
 if __name__ == "__main__":
