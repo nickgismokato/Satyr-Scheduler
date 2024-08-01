ee8681dc4d4fce54d5f205317471ec51c5c38ae4
nickgismokato <nickvillumlaursen@gmail.com>
2024-08-01 08:41:36 +0200

    Cleaned build output.

--- SatyrScheduler/build/lib/SatyrScheduler/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-from SatyrScheduler.GetData.GetData import GetData
-
-from SatyrScheduler.Schedule.Schedule import InitSchedule
-
-from SatyrScheduler.CustomTime.CustomTime import CreateTimeClass
